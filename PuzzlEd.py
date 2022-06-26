fbf7ffdad7879d8b3abf913b26dfa6cddc9cf935
= <eduardo.olivares@bluetab.net>
2022-06-26 18:47:21 +0200

    Edu is a square and moves in 2D

---
+++ PuzzlEd.py
@@ -0,0 +1,181 @@
+import pygame
+pygame.init()
+
+
+WIDTH, HEIGHT = 700, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pong")
+
+FPS = 60
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+PADDLE_WIDTH, PADDLE_HEIGHT = 50, 50
+BALL_RADIUS = 7
+
+SCORE_FONT = pygame.font.SysFont("comicsans", 50)
+WINNING_SCORE = 10
+
+
+class Paddle:
+    COLOR = WHITE
+    VEL = 4
+
+    def __init__(self, x, y, width, height):
+        self.x = self.original_x = x
+        self.y = self.original_y = y
+        self.width = width
+        self.height = height
+
+    def draw(self, win):
+        pygame.draw.rect(
+            win, self.COLOR, (self.x, self.y, self.width, self.height))
+
+    def move(self, direction):
+        if direction == "up":
+            self.y -= self.VEL
+        elif direction == "down":
+            self.y += self.VEL
+        elif direction == "left":
+            self.x -= self.VEL
+        elif direction == "right":
+            self.x += self.VEL
+
+    def reset(self):
+        self.x = self.original_x
+        self.y = self.original_y
+
+
+class Ball:
+    MAX_VEL = 5
+    COLOR = WHITE
+
+    def __init__(self, x, y, radius):
+        self.x = self.original_x = x
+        self.y = self.original_y = y
+        self.radius = radius
+        self.x_vel = self.MAX_VEL
+        self.y_vel = 0
+
+    def draw(self, win):
+        pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.radius)
+
+    def move(self):
+        self.x += self.x_vel
+        self.y += self.y_vel
+
+    def reset(self):
+        self.x = self.original_x
+        self.y = self.original_y
+        self.y_vel = 0
+        self.x_vel *= -1
+
+
+def draw(win, paddles, ball, left_score, right_score):
+    win.fill(BLACK)
+
+    left_score_text = SCORE_FONT.render(f"{left_score}", 1, WHITE)
+    right_score_text = SCORE_FONT.render(f"{right_score}", 1, WHITE)
+    win.blit(left_score_text, (WIDTH//4 - left_score_text.get_width()//2, 20))
+    win.blit(right_score_text, (WIDTH * (3/4) -
+                                right_score_text.get_width()//2, 20))
+
+    for paddle in paddles:
+        paddle.draw(win)
+
+    for i in range(10, HEIGHT, HEIGHT//20):
+        if i % 2 == 1:
+            continue
+        pygame.draw.rect(win, WHITE, (WIDTH//2 - 5, i, 10, HEIGHT//20))
+
+    ball.draw(win)
+    pygame.display.update()
+
+
+def handle_collision(ball, Edu):
+    if ball.y + ball.radius >= HEIGHT:
+        ball.y_vel *= -1
+    elif ball.y - ball.radius <= 0:
+        ball.y_vel *= -1
+
+    if ball.x_vel < 0:
+        if ball.y >= Edu.y and ball.y <= Edu.y + Edu.height:
+            if ball.x - ball.radius <= Edu.x + Edu.width:
+                ball.x_vel *= -1
+
+                middle_y = Edu.y + Edu.height / 2
+                difference_in_y = middle_y - ball.y
+                reduction_factor = (Edu.height / 2) / ball.MAX_VEL
+                y_vel = difference_in_y / reduction_factor
+                ball.y_vel = -1 * y_vel
+
+
+def handle_paddle_movement(keys, Edu):
+    if keys[pygame.K_w] and Edu.y - Edu.VEL >= 0: # HEIGHT = 0
+        Edu.move(direction = "up")
+    if keys[pygame.K_s] and Edu.y + Edu.VEL + Edu.height <= HEIGHT:
+        Edu.move(direction = "down")
+    if keys[pygame.K_a] and Edu.x - Edu.VEL >= 0: # WIDHT = 0
+        Edu.move(direction = "left")
+    if keys[pygame.K_d] and Edu.x + Edu.VEL + Edu.width <= WIDTH:
+        Edu.move(direction = "right")
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    Edu = Paddle(10, HEIGHT//2 - PADDLE_HEIGHT //
+                         2, PADDLE_WIDTH, PADDLE_HEIGHT)
+    ball = Ball(WIDTH // 2, HEIGHT // 2, BALL_RADIUS)
+
+    left_score = 0
+    right_score = 0
+
+    while run:
+        clock.tick(FPS)
+        draw(WIN, [Edu], ball, left_score, right_score)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+        keys = pygame.key.get_pressed()
+        handle_paddle_movement(keys, Edu)
+
+        ball.move()
+        handle_collision(ball, Edu)
+
+        if ball.x < 0:
+            right_score += 1
+            ball.reset()
+        elif ball.x > WIDTH:
+            left_score += 1
+            ball.reset()
+
+        won = False
+        if left_score >= WINNING_SCORE:
+            won = True
+            win_text = "Left Player Won!"
+        elif right_score >= WINNING_SCORE:
+            won = True
+            win_text = "Right Player Won!"
+
+        if won:
+            text = SCORE_FONT.render(win_text, 1, WHITE)
+            WIN.blit(text, (WIDTH//2 - text.get_width() //
+                            2, HEIGHT//2 - text.get_height()//2))
+            pygame.display.update()
+            pygame.time.delay(5000)
+            ball.reset()
+            Edu.reset()
+            left_score = 0
+            right_score = 0
+
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
---
+++ main.py
@@ -0,0 +1,41 @@
+# import the pygame module, so you can use it
+import pygame
+ 
+# define a main function
+def main():
+     
+    # initialize the pygame module
+    pygame.init()
+    # load and set the logo
+    logo = pygame.image.load("logo.jpg")
+    pygame.display.set_icon(logo)
+    pygame.display.set_caption("minimal program")
+     
+    # create a surface on screen that has the size of 240 x 180
+    screen = pygame.display.set_mode((1000, 600))
+    screen.fill((255, 255, 0))
+     
+    # define a variable to control the main loop
+    running = True
+
+    # main loop
+    while running:
+        # event handling, gets all event from the event queue
+        for event in pygame.event.get():
+            # only do something if the event is of type QUIT
+            if event.type == pygame.QUIT:
+                # change the value to False, to exit the main loop
+                running = False
+
+        image = pygame.image.load("logo.jpg")
+        image.set_alpha(128)
+        screen.blit(image, (50, 50)) 
+        image.set_colorkey((255,0,255)) # makes that color completely transparent so that the image appears not to be a rectangle    
+        pygame.display.flip()
+     
+     
+# run the main function only if this module is executed as the main script
+# (if you import this as a module then nothing is executed)
+if __name__=="__main__":
+    # call the main function
+    main()
