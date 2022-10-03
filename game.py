12d9c3bde273b40888935750d4d3d18fd8e6ffad
edusOmen <edu.likes.pringles@gmail.com>
2022-10-03 16:02:53 +0200

    can draw a red square

--- game.py
+++ game.py
@@ -4,6 +4,11 @@ from sys import exit
 pygame.init()
 screen = pygame.display.set_mode((1500,750))
 pygame.display.set_caption('PuzzlEd')
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((200, 200))
+test_surface.fill('Red')
+# pygame.draw.polygon()
 
 while True:
     for event in pygame.event.get():
@@ -13,4 +18,8 @@ while True:
 
     # draw all our elements
     # update everything
+
+    screen.blit(test_surface,(200, 200))
+
     pygame.display.update()
+    clock.tick(60)
