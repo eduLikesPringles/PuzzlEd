e96bb030908ecc8145db961f0799f5b03a898e13
edusOmen <edu.likes.pringles@gmail.com>
2022-10-03 16:22:01 +0200

    circular Ed drawn

--- game.py
+++ game.py
@@ -6,6 +6,32 @@ screen = pygame.display.set_mode((1500,750))
 pygame.display.set_caption('PuzzlEd')
 clock = pygame.time.Clock()
 
+WHITE = (255, 255, 255)
+
+class Ed:
+    COLOR = WHITE
+    VEL = 4
+
+    # we always start drawing a circle
+    def __init__(self, x, y, radius):
+        self.x =  x
+        self.y = y
+        self.radius = radius
+
+    def draw(self, win):
+        pygame.draw.circle(
+            screen, self.COLOR, (self.x, self.y), self.radius)
+
+    def move(self, up=True):
+        if up:
+            self.y -= self.VEL
+        else:
+            self.y += self.VEL
+
+    def reset(self):
+        self.x = self.original_x
+        self.y = self.original_y
+
 test_surface = pygame.Surface((200, 200))
 # test_surface.fill('Red')
 pygame.draw.polygon(test_surface, 'Red', [(100, 100), (100, 200), (200, 200)])
@@ -17,6 +43,10 @@ while True:
             exit()
 
     # draw all our elements
+
+    Edu = Ed(100, 100, 10)
+    Edu.draw(screen)
+
     # update everything
 
     screen.blit(test_surface,(200, 200))
