6d3bfa7ae47c6730646fe4fc87451380eac918fa
edusOmen <edu.likes.pringles@gmail.com>
2022-10-10 16:12:24 +0200

    random reshape, recolor and resize implemented

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 from sys import exit
 import math
+import random
 
 pygame.init()
 screen = pygame.display.set_mode((1500,750))
@@ -15,18 +16,23 @@ class Ed:
 
     # we always start drawing a circle
     def __init__(self, shape, x, y, size):
-        self.shape = shape
-        self.color = 'Red'
         self.x =  x
         self.y = y
         self.size = size
+        self.colors = ['Red', 'Green', 'Blue']
+        self.color = self.colors[0]
+        self.shapes = ['circle', 'square', 'triangle']
+        self.shape = self.shapes[0]
         # the size is the Area in units of px**2
 
+    def resize(self):
+        pass
+
     def recolor(self):
-        self.color = 'Blue'
+        self.color = random.choice(self.colors)
 
     def reshape(self):
-        self.shape = 'square'
+        self.shape = random.choice(self.shapes)
 
     def draw(self, win):
         if self.shape == 'circle':
@@ -34,10 +40,10 @@ class Ed:
                 screen, self.color, (self.x, self.y), math.sqrt(self.size/math.pi))
         elif self.shape == 'square':
             pygame.draw.rect(
-                screen, 'Blue', (self.x - math.sqrt(self.size) / 2, self.y - math.sqrt(self.size) / 2, math.sqrt(self.size), math.sqrt(self.size)))
+                screen, self.color, (self.x - math.sqrt(self.size) / 2, self.y - math.sqrt(self.size) / 2, math.sqrt(self.size), math.sqrt(self.size)))
         elif self.shape == 'triangle':
             pygame.draw.polygon(
-                screen, 'Red', [(self.x, self.y - math.sqrt(2 * math.sqrt(3) / 9 * self.size)), (self.x - 0.9 * (math.sqrt(self.size / math.sqrt(3))), (self.y + 0.9 * (math.sqrt(2 * math.sqrt(3) / 9 * self.size)))), (self.x + 0.9 * (math.sqrt(self.size / math.sqrt(3))), (self.y + 0.9 * (math.sqrt(2 * math.sqrt(3) / 9 * self.size))))])
+                screen, self.color, [(self.x, self.y - math.sqrt(2 * math.sqrt(3) / 9 * self.size)), (self.x - 0.9 * (math.sqrt(self.size / math.sqrt(3))), (self.y + 0.9 * (math.sqrt(2 * math.sqrt(3) / 9 * self.size)))), (self.x + 0.9 * (math.sqrt(self.size / math.sqrt(3))), (self.y + 0.9 * (math.sqrt(2 * math.sqrt(3) / 9 * self.size))))])
 
     def move(self, up=True):
         if up:
@@ -62,6 +68,13 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_presses = pygame.mouse.get_pressed()
+            if mouse_presses[0]:
+                Edu1.size += 1000
+            elif mouse_presses[2]:
+                Edu1.size -= 1000
+
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a]:
         Edu1.x -= 10
@@ -75,6 +88,10 @@ while True:
         Edu1.recolor()
     if keys[pygame.K_LCTRL]:
         Edu1.reshape()
+
+    buttons = pygame.mouse.get_pressed()
+    if buttons[pygame.BUTTON_LEFT]:
+        Edu1.size += 1000
 
     # draw all our elements
 
